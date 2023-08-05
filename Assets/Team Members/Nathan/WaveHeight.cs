ee7debe4c932a41b37b30610fbca24ade199aa52
NathanN3189 <nilsen.rotuma.nathan9@gmail.com>
2023-08-06 02:38:05 +1000

    water rising script and altered sky particle effect

---
+++ Assets/Team Members/Nathan/WaveHeight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveHeight : MonoBehaviour
+{
+    public Material waterMat;
+    public float height = 0.2f;
+    public float increaseHeightRate = 0.1f;
+    public float stopHeight = 0.9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        waterMat.SetFloat("_WaveHeight", height);
+        //WaterRise();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        height += increaseHeightRate * Time.deltaTime;
+        waterMat.SetFloat("_WaveHeight", height);
+
+
+
+        if ( height >= stopHeight)
+        {
+            height = stopHeight;
+            increaseHeightRate = 0f;
+        }
+    }
+    void WaterRise()
+    {
+        height += increaseHeightRate * Time.deltaTime;
+        waterMat.SetFloat("_WaveHeight", height);
+    }
+
+   
+
+
+
+}
